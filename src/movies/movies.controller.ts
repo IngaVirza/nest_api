a344bc1090e3ce2dc620ef6c017eb35e65159870
unknown <inha.virza@gmail.com>
2026-03-05 00:26:33 +0200

    controller with @Get, @Post, @Delete, @Patch

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -8,7 +8,21 @@ export class MoviesController {
   }
 
   @Get('/:id')
-  getOne() {
-    return 'film';
+  getOne(@Param('id') movieId: string) {
+    return `One film with id ${movieId}`;
+  }
+
+  @Post()
+  create() {
+    return 'This function create new film';
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This funvtion delete film with id ${movieId}`;
+  }
+  @Patch('/:id')
+  patch(@Param('id') movieId: string) {
+    return `This function will  edit film with id ${movieId}`;
   }
 }
