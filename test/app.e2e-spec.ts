0f9eee653584aaf22ad1b73d78f82bb9620169e3
unknown <inha.virza@gmail.com>
2026-03-11 23:46:27 +0200

    End-to-end testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -20,6 +20,10 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome in API Data base');
+  });
+
+  it('/movies (GET)', () => {
+    return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
   });
 });
